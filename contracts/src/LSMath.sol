d659ba324b6bec84a637994b45c0bb3b9213cf48
1BDO <vmoneymaker1@gmail.com>
2026-02-19 20:05:15 +0530

    length validation

--- contracts/src/LSMath.sol
+++ contracts/src/LSMath.sol
@@ -595,6 +595,10 @@ library LSMath {
         uint256[] memory initialQuantities,
         uint256 alpha
     ) internal pure returns (bool hasProfit, uint256 profit) {
+        // Validation: arrays must have same length and not be empty
+        if (quantities.length == 0 || initialQuantities.length == 0) revert EmptyQuantities();
+        if (quantities.length != initialQuantities.length) revert ArrayLengthMismatch();
+
         uint256 costCurrent = costFunction(quantities, alpha);
         uint256 costInitial = costFunction(initialQuantities, alpha);
 
